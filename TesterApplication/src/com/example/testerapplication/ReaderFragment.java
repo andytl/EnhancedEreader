e536b226a27d98858b5526b11243797918c4fea5
Shahar Levari <levaris@cs.washington.edu>
2015-04-09 13:06:44 -0700

    got arbitrary scroll on tap working

--- TesterApplication/src/com/example/testerapplication/ReaderFragment.java
+++ TesterApplication/src/com/example/testerapplication/ReaderFragment.java
@@ -3,14 +3,16 @@ package com.example.testerapplication;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.MotionEvent.PointerCoords;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ScrollView;
 import android.widget.TextView;
 
-public class ReaderFragment extends Fragment {
+public class ReaderFragment extends Fragment implements View.OnTouchListener {
 
-	
+	private int moveCount;
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -18,6 +20,7 @@ public class ReaderFragment extends Fragment {
 		super.onCreateView(inflater, container, savedInstanceState);
 		View rootView = inflater.inflate(R.layout.fragment_reader,
 				container, false);
+		moveCount = 0;
 		return rootView;
 	}
 	
@@ -29,6 +32,7 @@ public class ReaderFragment extends Fragment {
 	
 	private void displayText() {
 		ScrollView sv = (ScrollView) getView().findViewById(R.id.scroll_view);
+		sv.setOnTouchListener(this);
 		TextView tv = new TextView(getActivity());
 		String text = "";
 		for(int i = 0; i < 1000; i++) { 
@@ -38,5 +42,40 @@ public class ReaderFragment extends Fragment {
 		tv.setTextSize(20);
 		sv.addView(tv);
 	}
+	
+
+	public void centerScreen(int x, int y) {
+		ScrollView sv = (ScrollView) getView().findViewById(R.id.scroll_view);
+		int left = 	sv.getLeft();
+		int top = sv.getTop();
+		int right = sv.getRight();
+		int bottom = sv.getBottom();
+		int centerX = (right-left)/2;
+		int centerY = (bottom-top)/2;
+		int diffX = x-centerX;
+		diffX = 0;
+		int diffY = y-centerY;
+		sv.scrollBy(diffX, diffY);
+	}
+
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		int id = v.getId();
+		int action = event.getAction();
+		if (id == R.id.scroll_view) {
+			if (action == MotionEvent.ACTION_UP) {
+				if (moveCount < 5) {
+	 				PointerCoords pc0 = new PointerCoords();
+					event.getPointerCoords(0, pc0);
+					centerScreen((int)pc0.x, (int)pc0.y);
+				}
+				moveCount = 0;
+			} else if (action == MotionEvent.ACTION_MOVE){
+				moveCount++;
+			}
+		}
+		return false;
+	}
+	
 	
 }
