a5ebffdcc83d227ffc68544ff18fe3d84a8356c0
Shahar Levari <levaris@cs.washington.edu>
2015-04-20 23:08:07 -0700

    added scroll policy and blur policy

--- TesterApplication/src/com/example/testerapplication/ReaderActivity.java
+++ TesterApplication/src/com/example/testerapplication/ReaderActivity.java
@@ -13,6 +13,8 @@ import android.os.Build;
 
 public class ReaderActivity extends Activity {
 
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
