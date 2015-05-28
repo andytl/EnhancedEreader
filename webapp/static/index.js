393fe792ac05cce80cd107ca2b2cc57e1ead64c4
Andrew Lam <andytl@cs.washington.edu>
2015-05-28 14:16:57 -0700

    fancy some good stuff

--- webapp/static/index.js
+++ webapp/static/index.js
@@ -7,16 +7,67 @@ window.onload = function() {
 function loadUsers() {
   $.get('api/users')
     .done(function(data) {
-      $('#userlist').append($('<select>').addClass('form-control')
+      $('#userlist').html('').append($('<select>').addClass('form-control')
           .append(data.map(function(d) {
-            return $('<option>').html(d);
-          })));
+            return $('<option>').html(d).val(d);
+          })).on('change', loadUser));
     })
     .fail(function(err) {
       console.log(err);
     });
 }
 
+function loadUser() {
+  var username = $(this).val();
+  $.get('api/entry/' + username)
+    .done(function(data) {
+      var svg = $('<svg>');
+      $('#visualization').empty().append(svg);
+      var series = {
+        key: data.username,
+        values: data.entries.map(function(d) {
+            return {
+              x: d.timestamp,
+              y: d.focusrate
+            };
+          })
+      };
+      plotData([series], svg[0]);
+    })
+    .fail(function(err) {
+      console.log(err);
+    });
+}
 
+function plotData(data, domElt) {
+  nv.addGraph(function() {
+    var chart = nv.models.lineChart()
+                  .margin({left: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                  .useInteractiveGuideline(true)  //We want nice looking tooltips and a guideline!
+//                  .transitionDuration(350)  //how fast do you want the lines to transition?
+                  .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                  .showYAxis(true)        //Show the y-axis
+                  .showXAxis(true)        //Show the x-axis
+    ;
+
+    chart.xAxis     //Chart x-axis settings
+        .axisLabel('Timestamp (ms, epoch)')
+        .tickFormat(d3.format(',r'));
+
+    chart.yAxis     //Chart y-axis settings
+        .axisLabel('Focusrate (?)')
+        .tickFormat(d3.format('.02f'));
+
+    /* Done setting the chart up? Time to render it!*/
+
+    d3.select(domElt)    //Select the <svg> element you want to render the chart in.   
+        .datum(data)           //Populate the <svg> element with chart data...
+        .call(chart);          //Finally, render the chart!
+
+    //Update the chart when window resizes.
+    nv.utils.windowResize(function() { chart.update(); });
+    return chart;
+  });
+}
 
 })();
