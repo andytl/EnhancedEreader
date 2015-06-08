25577e5e3ac020417844399187508d5b161a32e0
Andrew Lam <andytl@cs.washington.edu>
2015-06-08 00:26:04 -0700

    improved random data generation algs

--- webapp/randomdata.js
+++ webapp/randomdata.js
@@ -2,7 +2,11 @@
 "use strict";
 
 /*
+ * This endpoint generates some random data for the API to aid in testing
+ * of the application.
  */
+
+var util = require('util');
 
 var chance = require('chance').Chance();
 var bodyParser = require('body-parser');
@@ -39,15 +43,26 @@ router.post('/entry', function (req, res, next) {
     return;
   }
   var created = [];
-  for (var i = 0; i < params.n; i++) {
-    created.push({
-      timestamp: i * params.i + parseInt(params.s, 10) + 1433709320362,
-      focusrate: chance.floating({min: 0, max: 1, floating: 3}),
-      totaltime: chance.integer({min: 360000, max: 3600000}),
-      timereading: chance.integer({min: 136000, max: 1200000}),
-      dartingrate: chance.floating({min: 0, max: 1, floating: 3})
-    });
+  var data = {
+    timestamp: parseInt(params.s, 10) + 1433709320362,
+    focusrate: chance.floating({min: 0, max: 1, floating: 3}),
+    totaltime: chance.integer({min: 360000, max: 3600000}),
+    timereading: chance.integer({min: 136000, max: 1200000}),
+    dartingrate: chance.floating({min: 0, max: 1, floating: 3})
+  };
+  created.push(data);
+  for (var i = 1; i < params.n; i++) {
+    var newdata = {
+      timestamp: data.timestamp + parseInt(params.i, 10),
+      focusrate: nextRandom(data.focusrate, 0, 1, 0.15, 3),
+      totaltime: nextRandom(data.totaltime, 360000, 3600000, 36000, 0),
+      timereading: nextRandom(data.timereading, 136000, 1200000, 13600, 0),
+      dartingrate: nextRandom(data.dartingrate, 0, 1, 0.15, 3)
+    };
+    created.push(newdata);
+    data = newdata;
   }
+  console.log(util.inspect(created));
   database.addEntries(
       created.map(function (e) {
         e.username = params.u;
@@ -60,6 +75,12 @@ router.post('/entry', function (req, res, next) {
       })
   );
 });
+
+function nextRandom(input, min, max, stddev, places) {
+  var temp = input + chance.normal({ dev: stddev });
+  var output = Math.min(max, Math.max(min, temp));
+  return Math.round(output * Math.exp(10, places)) / Math.exp(10, places);
+}
 
 // Datafctn should take the database data and return the json to send back
 function confirmResponse(req, res, next, response) {
